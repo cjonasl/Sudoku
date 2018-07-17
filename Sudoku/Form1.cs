5fafdff3619511be87b0143899d284be31ecf515
Jonas <jonas.leander@teamdatalogic.com>
2018-07-17 11:22:04 +0100

    Commit 2018-07-17 kl. 11:22

---
+++ Sudoku/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class SudokuBoard
+    {
+        private int[,] _rows, _columns, _squares;
+        private int _numberOfBoardEntriesSet;
+
+        public SudokuBoard()
+        {
+            _rows = new int[9, 9];
+            _columns = new int[9, 9];
+            _squares = new int[9, 9];
+            _numberOfBoardEntriesSet = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    _rows[i, j] = 0;
+                    _columns[i, j] = 0;
+                    _squares[i, j] = 0;
+                }
+            }
+        }
+
+        public bool RowHasNumberInAnyEntry(int rowIndex, int number)
+        {
+            bool returnValue = false;
+            int i = 0;
+
+            if ((rowIndex < 0) || (rowIndex > 8))
+            {
+                throw new Exception("Error in method RowHasNumberInAnyEntry! Row index must be an integer between 0 and 8.");
+            }
+
+            if ((number < 1) || (number > 9))
+            {
+                throw new Exception("Error in method RowHasNumberInAnyEntry! Number to check must be an integer between 1 and 9.");
+            }
+
+            while ((!returnValue) && (i < 9))
+            {
+                if (_rows[rowIndex, i] == number)
+                {
+                    returnValue = true;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return returnValue;
+        }
+
+        public bool ColumnHasNumberInAnyEntry(int columnIndex, int number)
+        {
+            bool returnValue = false;
+            int i = 0;
+
+            if ((columnIndex < 0) || (columnIndex > 8))
+            {
+                throw new Exception("Error in method ColumnHasNumberInAnyEntry! Column index must be an integer between 0 and 8.");
+            }
+
+            if ((number < 1) || (number > 9))
+            {
+                throw new Exception("Error in method ColumnHasNumberInAnyEntry! Number to check must be an integer between 1 and 9.");
+            }
+
+            while ((!returnValue) && (i < 9))
+            {
+                if (_columns[columnIndex, i] == number)
+                {
+                    returnValue = true;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return returnValue;
+        }
+
+        public bool SquareHasNumberInAnyEntry(int squareIndex, int number)
+        {
+            bool returnValue = false;
+            int i = 0;
+
+            if ((squareIndex < 0) || (squareIndex > 8))
+            {
+                throw new Exception("Error in method SquareHasNumberInAnyEntry! Square index must be an integer between 0 and 8.");
+            }
+
+            if ((number < 1) || (number > 9))
+            {
+                throw new Exception("Error in method SquareHasNumberInAnyEntry! Number to check must be an integer between 1 and 9.");
+            }
+
+            while ((!returnValue) && (i < 9))
+            {
+                if (_squares[squareIndex, i] == number)
+                {
+                    returnValue = true;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return returnValue;
+        }
+
+        public void SetNumber(int rowIndex, int columnIndex, int number)
+        {
+            int squareIndex, squareSequenceIndex;
+
+            if (_rows[rowIndex, columnIndex] != 0)
+            {
+                throw new Exception("Error in method SetNumber! A number is already set in row " + (rowIndex + 1).ToString() + " column " + (columnIndex + 1).ToString());
+            }
+
+            if ((rowIndex < 0) || (rowIndex > 8))
+            {
+                throw new Exception("Error in method SetNumber! Row index must be an integer between 0 and 8.");
+            }
+
+            if ((columnIndex < 0) || (columnIndex > 8))
+            {
+                throw new Exception("Error in method SetNumber! Column index must be an integer between 0 and 8.");
+            }
+
+            if ((number < 1) || (number > 9))
+            {
+                throw new Exception("Error in method SetNumber! Number to set must be an integer between 1 and 9.");
+            }
+
+            if (RowHasNumberInAnyEntry(rowIndex, number))
+            {
+                throw new Exception("Error in method SetNumber! There is alreday a number " + number.ToString() + " in row " + (rowIndex + 1).ToString());
+            }
+
+            if (ColumnHasNumberInAnyEntry(columnIndex, number))
+            {
+                throw new Exception("Error in method SetNumber! There is alreday a number " + number.ToString() + " in column " + (columnIndex + 1).ToString());
+            }
+
+            squareIndex = (3 * (rowIndex / 3)) + (columnIndex / 3);
+
+            if (SquareHasNumberInAnyEntry(squareIndex, number))
+            {
+                throw new Exception("Error in method SetNumber! There is alreday a number " + number.ToString() + " in squre " + (squareIndex + 1).ToString());
+            }
+
+            _rows[rowIndex, columnIndex] = number;
+            _columns[columnIndex, rowIndex] = number;
+
+            squareSequenceIndex = (3 * (rowIndex % 3)) + (columnIndex % 3);
+            _squares[squareIndex, squareSequenceIndex] = number;
+
+            _numberOfBoardEntriesSet++;
+        }
+    }
+}
